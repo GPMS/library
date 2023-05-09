32838b2a16851c55987eddcb0172f203502ba4bc
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2023-05-09 19:54:06 -0300

    Add new book to library

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const formDialog = document.querySelector("[data-dialog]");
+const form = document.querySelector("[data-form]");
+const authorField = document.getElementById("author");
+const titleField = document.getElementById("title");
+const pageCountField = document.getElementById("page-count");
+const closeDialogBtn = document.querySelector("[data-close-dialog-btn]");
+
 const bookDisplay = document.querySelector("[data-book-display]");
 const bookTemplate = document.querySelector("[data-book-template]");
+const addBookTemplate = document.querySelector("[data-add-book-template]");
 const myLibrary = [];
 
 function Book(author, title, pageCount, read) {
@@ -23,6 +31,38 @@ function displayLibrary() {
 
     bookDisplay.append(bookView);
   }
+  const addBook = addBookTemplate.content.cloneNode(true);
+  bookDisplay.append(addBook);
+  const addBookBtn = document.querySelector("[data-add-btn]");
+  addBookBtn.addEventListener("click", () => {
+    formDialog.showModal();
+  });
 }
+
+closeDialogBtn.addEventListener("click", () => {
+  formDialog.close();
+});
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  displayLibrary();
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = titleField.value;
+  const author = authorField.value;
+  const pageCount = pageCountField.value;
+  if (title.length == 0 || author.length == 0 || pageCount <= 0) {
+    alert("Invalid book");
+    return;
+  }
+  const newBook = new Book(author, title, pageCount, read);
+  addBookToLibrary(newBook);
+  authorField.value = "";
+  titleField.value = "";
+  pageCountField.value = 0;
+  formDialog.close();
+});
 
 displayLibrary();
